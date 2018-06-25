c75e834b3ffc40487ab3ea467a97ebcb942ebb36
Jen Murphy <32068518+jendmurphy@users.noreply.github.com>
2018-06-25 10:40:57 +0100

    PLots

--- Day 1 R script.R
+++ Day 1 R script.R
@@ -5,3 +5,61 @@ setwd("//stfdata07/home/MI/Mip18jdm/ManW10/Desktop/SHEFFIELD/SHEFFIELD")
 install.packages("tidyverse")
 library("tidyverse")
 
+#  Make an empty ggplot
+ggplot()
+
+#  Line chart
+ggplot(data = economics,
+       aes(x=date,y=unemploy))+
+  geom_line()
+
+#  Bar chart
+ggplot(data = midwest,
+       aes(x=state))+
+  geom_bar()
+
+#  histogram - specified bin
+ggplot(data = diamonds,
+       aes(x=price))+
+  geom_histogram(bins = 10)
+
+#  histogram - specified bin width
+ggplot(data = diamonds,
+       aes(x=price))+
+  geom_histogram(binwidth = 500) # this specifies each bin as $500 dollars width
+
+#  density
+ggplot (data = diamonds,
+        aes(x=price))+
+  geom_density()
+
+#  frequency polygon
+ggplot (data = diamonds,
+        aes(x=price))+
+  geom_freqpoly()
+
+#  Plotting two graphs on one set of axes
+#  frequency polygon
+ggplot (data = diamonds,
+        aes(x=price))+
+  geom_freqpoly()+
+  geom_histogram(alpha = .3) #  change transparency  plot both on one plot
+
+#  BOXPLOT
+#  Now working with the diamonds data to plot the distribution of a continuous variable, by a categorical variable - BOXPLOT
+ggplot(data=diamonds,
+       aes(y=price, x=color))+
+  geom_boxplot()
+
+#  VIOLIN
+#  Now working with the diamonds data to plot the distribution of a continuous variable, by a categorical variable - VIOLIN 
+ggplot(data=diamonds,
+       aes(y=price, x=color))+
+  geom_violin()
+
+#  DENSITY PLOT - different lines are different colours.  ggplot will take colour or color as a spelling (note that diamonds dataset includes a variable 'color' which is why it is spelt both ways here)
+ggplot(data=diamonds,
+       aes(x=price, colour=color))+
+  geom_density()
+
+
