ee5efbbfee2171dd118b54d5c3e678907cf99e54
jendmurphy <32068518+jendmurphy@users.noreply.github.com>
2018-07-04 08:37:35 +0100

    Add kaggle code

---
+++ Kaggle Survey.R
@@ -0,0 +1,7 @@
+setwd("~/PHD/Sheffield")
+
+conversionrates <- read.csv("conversionRates.csv")
+freeform <- read.csv("freeformResponses.csv")
+multiple <- read.csv("multipleChoiceResponses.csv")
+schema <- read.csv("schema.csv")
+
